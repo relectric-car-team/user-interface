9c87d327f9b83b5696bd0bc4b79e69604ec64705
Ratik Kapoor <ratikrk@gmail.com>
2020-11-22 18:47:13 -0700

    Test feature 1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -14,7 +14,7 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Tab 1 Ratik says hello</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
