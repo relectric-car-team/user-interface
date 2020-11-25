e4b6d07598d732407c9852628972f716a3097db3
dhyey <dhyeylalseta@outlook.com>
2020-11-25 12:22:06 -0700

    dhyey says hello

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -14,7 +14,7 @@ const Tab1: React.FC = () => {
             <IonContent fullscreen>
                 <IonHeader collapse="condense">
                     <IonToolbar>
-                        <IonTitle size="large">Tab 1 Thomas says hello</IonTitle>
+                        <IonTitle size="large">Tab 1 Dhyey says hello</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <ExploreContainer name="Brooke's 1 page" />
