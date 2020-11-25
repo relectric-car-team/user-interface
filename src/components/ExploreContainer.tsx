56055cd63fd84663cb07fcca1f766542246ff11f
Ratik Kapoor <ratikrk@gmail.com>
2020-11-25 11:29:28 -0700

    Add ESLint and Prettier, add PR checks

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
-  name: string;
+    name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+const ExploreContainer: React.FC<ContainerProps> = (props: ContainerProps) => {
+    return (
+        <div className="container">
+            <strong>{props.name}</strong>
+            <p>
+                Explore{' '}
+                <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">
+                    UI Components
+                </a>
+            </p>
+        </div>
+    );
 };
 
 export default ExploreContainer;
