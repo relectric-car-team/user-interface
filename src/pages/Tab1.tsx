74035a519db27151e9f24b54cc9531f2be7e4936
Brooke Kindleman <brooke.kindleman@gmail.com>
2020-11-25 12:30:27 -0700

    Update Tab1.tsx

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -17,7 +17,7 @@ const Tab1: React.FC = () => {
                         <IonTitle size="large">Tab 1 Brooke says hello</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <ExploreContainer name="Tab 1 page" />
+                <ExploreContainer name="Brooke's 1 page" />
             </IonContent>
         </IonPage>
     );
