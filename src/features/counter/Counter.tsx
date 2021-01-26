da9db529f3303337667ad4de59862fb94ced88a3
Ratik Kapoor <ratikrk@gmail.com>
2021-01-26 16:08:03 -0700

    Revert change to SCSS due to ARM incompatibility

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment, incrementByAmount, incrementAsync, selectCount } from './counterSlice';
 import styles from './Counter.module.css';
-import { IonContent } from '@ionic/react';
 
 export function Counter() {
     const count = useSelector(selectCount);
     const dispatch = useDispatch();
     const [incrementAmount, setIncrementAmount] = useState('2');
-    //TODO: Use Ionic components instead of HTML tags
+
     return (
-        <IonContent>
+        <div>
             <div className={styles.row}>
                 <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
                     +
@@ -40,6 +39,6 @@ export function Counter() {
                     Add Async
                 </button>
             </div>
-        </IonContent>
+        </div>
     );
 }
