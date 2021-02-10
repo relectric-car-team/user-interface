8faf7e178724d456083d3e8f1615bdb0022b894f
Ratik Kapoor <ratikrk@gmail.com>
2021-02-09 20:49:18 -0700

    Create Enums.ts

---
+++ src/Models/Enums.ts
@@ -0,0 +1,22 @@
+/**
+ * Enums.ts
+ *
+ * This file is used to to store enumerations to be used for type checking across various components
+ */
+
+/**
+ * Enum: Pages
+ *
+ * Used for type-strict enforcement of pages
+ *
+ * @author Ratik Kapoor
+ * @since 0.0.2
+ */
+export enum Pages {
+    Home = 'Home',
+    Car = 'Car',
+    Climate = 'Climate',
+    Music = 'Music',
+    Navigation = 'Navigation',
+    Settings = 'Settings',
+}
