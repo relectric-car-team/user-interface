7f2a2dcac83431e3d26c198abd85876cd09b3016
Elize <elize.tran@hotmail.com>
2021-03-01 11:38:00 -0700

    finished wip

--- src/pages/Climate.tsx
+++ src/pages/Climate.tsx
@@ -5,7 +5,6 @@ import {
     IonPage,
     IonTitle,
     IonToolbar,
-    IonIcon,
     IonCard,
     IonCardContent,
     IonSegment,
@@ -13,10 +12,24 @@ import {
     IonLabel,
     IonCol,
 } from '@ionic/react';
-import { carOutline, flameOutline } from 'ionicons/icons';
+
+/**
+ * Imports for all custom icons
+ */
 import climateUpper from '../assets/icons/climate-upper.png';
+import climateUpperOn from '../assets/icons/climate-upper-on.png';
+import climateLower from '../assets/icons/climate-lower.png';
+import climateLowerOn from '../assets/icons/climate-lower-on.png';
+import climateUpperAndLower from '../assets/icons/climate-upper-and-lower.png';
+import climateUpperAndLowerOn from '../assets/icons/climate-upper-and-lower-on.png';
+import climateFront from '../assets/icons/climate-front.png';
+import climateFrontOn from '../assets/icons/climate-front-on.png';
+import fan from '../assets/icons/fan-on.png';
 import './Climate.css';
 
+/**
+ * Enums for consistent identification of the direction mode selected
+ */
 enum Direction {
     Upper = 'Upper',
     Lower = 'Lower',
@@ -24,43 +37,43 @@ enum Direction {
     Front = 'Front',
 }
 
+/**
+ * Enums for consistent identification of the intensity mode selected
+ */
+enum Intensity {
+    off = 'OFF',
+    one = '1',
+    two = '2',
+    three = '3',
+    four = '4'
+}
+
+/**
+ * The climate tab manages the selection choice of the user for intensity of airflow 
+ * and direction of airflow. The temperature of the vehicle interior is also displayed
+ * on this tab. 
+ */
 const Climate: React.FC = () => {
-    // dummy varaible to represent temp measurement
-    const [selectedDirection, setSelectedDirection] = useState<Direction>(Direction.Upper);
+    
+    const [selectedDirection, setSelectedDirection] = useState<Direction>();
+    const [selectedIntensity, setSelectedIntensity] = useState<Intensity>();
+    
+    // dummy varaible to represent interior temperature measurement
     const temp = 22;
 
-    const setDirection = (d: string | undefined) => {
-        switch (d) {
-            case Direction.Front:
-                setSelectedDirection(Direction.Front);
-                break;
-
-            case Direction.Lower:
-                setSelectedDirection(Direction.Lower);
-                break;
-
-            case Direction.Upper:
-                setSelectedDirection(Direction.Upper);
-                break;
-
-            case Direction.UpperAndLower:
-                setSelectedDirection(Direction.UpperAndLower);
-                break;
-
-            default:
-                setSelectedDirection(Direction.Upper);
-                break;
-        }
-    };
 
     return (
         <IonPage>
             <IonHeader>
+
+                {/*
+                This toolbar is the at the tope of the modal and displays the name of the current
+                tab being views, i.e. Climate
+                */}
                 <IonToolbar color="#D99648" className="ClimateToolBar">
                     <IonTitle size="large" color="white">
                         Climate
                     </IonTitle>
-                    0
                 </IonToolbar>
             </IonHeader>
 
@@ -71,57 +84,154 @@ const Climate: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
 
+
+                {/*
+                DirectionCard is the uppermost button panel which accpets selection of choice 
+                of direction of airflow: upper passenger body, lower passenger body, both upper
+                and lower, and front of vehicle/windshield. Only one mode may be selected at 
+                a time.
+                */}
                 <IonCard className="DirectionCard" color="light">
                     <IonCardContent>
-                        <IonSegment
-                            value={selectedDirection}
-                            onIonChange={(e) => {
-                                setDirection(e.detail.value);
-                            }}
-                        >
-                            <IonSegmentButton value={Direction.Upper} className="SegmentButton">
-                                <img src={climateUpper} className="climateUpper" />
+
+                        {/*
+                        Buttons on the same panel are contained within segments to only allow 
+                        one button to be selected at any given time.
+                        */}
+                        <IonSegment value={selectedDirection}>
+
+                            {/*
+                            (Left) button indicating airflow in the upper body direction
+                            */}
+                            <IonSegmentButton 
+                                value={Direction.Upper} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedDirection(Direction.Upper)}>
+                                    {selectedDirection == Direction.Upper &&
+                                    selectedIntensity != Intensity.off ? 
+                                        <img src={climateUpperOn} className="ClimateUpperButton" /> : 
+                                        <img src={climateUpper} className="ClimateUpperButton" />}
                             </IonSegmentButton>
-                            <IonSegmentButton value={Direction.Lower} className="SegmentButton">
-                                <IonIcon name={flameOutline} />
+
+                            {/*
+                            (2nd from left) button indicating airflow in the lower body direction
+                            */}
+                            <IonSegmentButton 
+                                value={Direction.Lower} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedDirection(Direction.Lower)}>
+                                    {selectedDirection == Direction.Lower &&
+                                    selectedIntensity != Intensity.off ?  
+                                        <img src={climateLowerOn} className="DirectionButton" /> : 
+                                        <img src={climateLower} className="DirectionButton" />}
                             </IonSegmentButton>
-                            <IonSegmentButton value={Direction.UpperAndLower} className="SegmentButton">
-                                <IonIcon icon={carOutline} />
+
+                            {/*
+                            (2nd from right) button indicating airflow in the upper and lower body direction
+                            */}
+                            <IonSegmentButton 
+                                value={Direction.UpperAndLower} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedDirection(Direction.UpperAndLower)}>
+                                    {selectedDirection == Direction.UpperAndLower &&
+                                    selectedIntensity != Intensity.off ? 
+                                        <img src={climateUpperAndLowerOn} className="DirectionButton" /> : 
+                                        <img src={climateUpperAndLower} className="DirectionButton" />}
                             </IonSegmentButton>
-                            <IonSegmentButton value={Direction.Front} className="SegmentButton">
-                                <IonIcon icon={carOutline} />
+
+                            {/*
+                            (Right) button indicating airflow in the windshield's direction
+                            */}
+                            <IonSegmentButton 
+                                value={Direction.Front} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedDirection(Direction.Front)}>
+                                    {selectedDirection == Direction.Front &&
+                                    selectedIntensity != Intensity.off ? 
+                                        <img src={climateFrontOn} className="DirectionButton" /> : 
+                                        <img src={climateFront} className="DirectionButton" />}
                             </IonSegmentButton>
                         </IonSegment>
                     </IonCardContent>
                 </IonCard>
 
+
+                {/*
+                Intensity is the lower button panel which accpets selection of choice of
+                intensity of airflow: OFF, 1, 2, 3, and 4. OFF is the least intense with no
+                airflow while 4 is the most intense mose. Only one mode may be selected at 
+                a time.
+                */}
                 <IonCard className="IntensityCard" color="light">
                     <IonCardContent>
-                        <IonSegment value="OFF">
-                            <IonSegmentButton value="OFF" className="SegmentButton">
+
+                        {/*
+                        Buttons on the same panel are contained within segments to only allow 
+                        one button to be selected at any given time.
+                        */}
+                        <IonSegment value={selectedIntensity}>
+
+                            {/*
+                            (Left) button indicating airflow is off
+                            */}
+                            <IonSegmentButton 
+                            value={Intensity.off} 
+                            className="SegmentButton"
+                            onClick={() => setSelectedIntensity(Intensity.off)}>
                                 <IonLabel>OFF</IonLabel>
                             </IonSegmentButton>
-                            <IonSegmentButton value="1" className="SegmentButton">
-                                <IonLabel>1</IonLabel>
+
+                            {/*
+                            (2nd from left) button indicating airflow is low; intensity = 1
+                            */}
+                            <IonSegmentButton 
+                                value={Intensity.one} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedIntensity(Intensity.one)}>
+                                    <IonLabel>1</IonLabel>
                             </IonSegmentButton>
-                            <IonSegmentButton value="2" className="SegmentButton">
-                                <IonLabel>2</IonLabel>
+
+                            {/*
+                            (Middle) button indicating airflow is medium; intensity = 2
+                            */}
+                            <IonSegmentButton 
+                                value={Intensity.two} 
+                                    className="SegmentButton"
+                                    onClick={() => setSelectedIntensity(Intensity.two)}>
+                                        <IonLabel>2</IonLabel>
                             </IonSegmentButton>
-                            <IonSegmentButton value="3" className="SegmentButton">
-                                <IonLabel>3</IonLabel>
+
+                            {/*
+                            (2nd from right) button indicating airflow is med-high; intensity = 3
+                            */}
+                            <IonSegmentButton 
+                                value={Intensity.three} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedIntensity(Intensity.three)}>
+                                    <IonLabel>3</IonLabel>
                             </IonSegmentButton>
-                            <IonSegmentButton value="4" className="SegmentButton">
-                                <IonLabel>4</IonLabel>
+
+                            {/*
+                            (Right) button indicating airflow is high; intensity = 4
+                            */}
+                            <IonSegmentButton 
+                                value={Intensity.four} 
+                                className="SegmentButton"
+                                onClick={() => setSelectedIntensity(Intensity.four)}>
+                                <   IonLabel>4</IonLabel>
                             </IonSegmentButton>
                         </IonSegment>
                     </IonCardContent>
                 </IonCard>
 
+                {/*
+                Bottom right panel displaying current interior temperature of the vehicle.
+                */}
                 <IonCard className="TempCard" color="light">
                     <IonLabel color="medium">{temp}</IonLabel>
                     <IonLabel color="medium">°C</IonLabel>
-                    <IonCol size="2"> </IonCol>
-                    <IonIcon color="medium" size="large" vertical-align="bottom" icon={flameOutline} />
+                    <IonCol size="1"> </IonCol>
+                    <img src={fan} className="Fan" />
                 </IonCard>
             </IonContent>
         </IonPage>
