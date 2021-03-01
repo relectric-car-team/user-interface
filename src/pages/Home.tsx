95cd294269d3acaaa73899654b5dc328c8d7716e
Brooke <brooke.kindleman@gmail.com>
2021-02-28 21:15:39 -0700

    Added React hook for getting the current time to display on the home page, updating every second. Set CSS formatting to make the time appear in the middle of the screen with a color matching the theme. All code for the time hook is located in the home page, but could definitely be moved. CSS code might need to be moved to its own file - it is currently stored in the ComponentModal.css file.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import './ComponentModal.css';
+
+function formatTime(originalTime: string) {
+    let updateTime = originalTime;
+
+    if (parseInt(originalTime.substring(0, 2)) > 12) {
+        const oldHour = parseInt(originalTime.substring(0, 2));
+        const newHour = oldHour - 12;
+
+        updateTime = originalTime.replace(oldHour.toString(), newHour.toString());
+    }
+
+    return updateTime;
+}
+
+function getTime() {
+    const [currentTime, setTime] = useState(formatTime(new Date().toLocaleTimeString()));
+
+    useEffect(() => {
+        const secTimer = setInterval(() => {
+            setTime(formatTime(new Date().toLocaleTimeString()));
+        }, 1000);
+
+        return () => clearInterval(secTimer);
+    }, []);
+
+    return currentTime;
+}
 
 const Home: React.FC = () => {
     return (
@@ -17,7 +44,7 @@ const Home: React.FC = () => {
                         {/* TODO: Add time here */}
                     </IonToolbar>
                 </IonHeader>
-                <ExploreContainer name="Home" />
+                <div className="time">{getTime()}</div>
             </IonContent>
         </IonPage>
     );
