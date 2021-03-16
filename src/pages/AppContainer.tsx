3f43a399139de265636ec5dfd3703e9ff512ca18
Ratik Kapoor <ratikrk@gmail.com>
2021-03-16 14:57:34 -0600

    docs: Added inline docs to components

--- src/pages/AppContainer.tsx
+++ src/pages/AppContainer.tsx
@@ -7,15 +7,22 @@ import ComponentModal from './ComponentModal';
 const AppContainer: React.FC = () => {
     const [currentPage, setCurrentPage] = useState<Pages>(Pages.Home);
 
+    /**
+     * Sets current page for UI. Uncomment the console.log statement to debug
+     * @param page Page to set UI to
+     */
     const setPage = (page: Pages) => {
-        console.log(page);
+        // console.log(page);
         setCurrentPage(page);
     };
 
     return (
         <>
+            {/* Overlaying modal component */}
             <ComponentModal page={currentPage} pageCallback={setPage} />
+            {/* Home page */}
             <Home />
+            {/* Lower tab bar with icon */}
             <TabBar pageCallback={setPage} currentPage={currentPage} />
         </>
     );
