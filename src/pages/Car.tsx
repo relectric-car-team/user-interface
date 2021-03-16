353f64bcf0d5e748d8e4bdb77420bd6fd35d4df7
Thomas Kahessay <Kahessaythomas@gmail.com>
2021-03-16 14:45:24 -0600

    Updated statistics

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -1,23 +1,99 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonIcon,
+    IonModal,
+} from '@ionic/react';
+import { batteryHalfOutline } from 'ionicons/icons';
 import carAerial from '../assets/icons/car-aerial.png';
+import energyGraph from '../assets/icons/Energy.jpg';
+import batteryGraph from '../assets/icons/BatteryGraph.jpg';
 import './Car.css';
+import './InnerModal.css';
+import Energy from './CarModals/Energy';
+import Battery from './CarModals/Battery';
 
 const Car: React.FC = () => {
+    const [showEnergy, setShowEnergy] = useState(false);
+    const [showBattery, setShowBattery] = useState(false);
+
+    const openEnergyModal = function () {
+        setShowEnergy(true);
+    };
+
+    const openBatteryModal = function () {
+        setShowBattery(true);
+    };
+
     return (
-        <IonPage>
+        <IonPage className="CarPage">
+            {/* Tool bar header for Car page */}
             <IonHeader>
                 <IonToolbar color="#099648" className="CarToolBar">
-                    <IonTitle>Car View</IonTitle>
+                    <IonTitle>Car</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent fullscreen>
+            <IonContent fullscreen scrollY={false}>
                 <IonHeader collapse="condense">
                     <IonToolbar>
                         <IonTitle size="large">Car Tab</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <IonImg className="AerialView" src={carAerial} />
+                {/* Grid used to partition each area of the page */}
+                <IonGrid>
+                    <IonRow>
+                        {/* Modal to display the energy on click */}
+                        <IonModal isOpen={showEnergy} onDidDismiss={() => setShowEnergy(false)} cssClass="InnerModal">
+                            <Energy></Energy>
+                        </IonModal>
+                        {/* Left Side: Statistics Fuel efficiency/Energy Consumption */}
+                        <IonCol className="LeftStats">
+                            <IonCard className="FuelEfficiency" button={true} onClick={openEnergyModal}>
+                                <IonImg src={energyGraph} className="ImagePreview" />
+                                <IonCardHeader>
+                                    <IonCardTitle>Energy</IonCardTitle>
+                                    <IonCardSubtitle>Range: 400km</IonCardSubtitle>
+                                    <IonCardSubtitle>Consumption: 15kWh/100km</IonCardSubtitle>
+                                </IonCardHeader>
+                                <IonCardContent></IonCardContent>
+                            </IonCard>
+                        </IonCol>
+                        {/* Middle Section: Car View */}
+                        <IonCol>
+                            <IonImg className="AerialView" src={carAerial} />
+                        </IonCol>
+                        {/* Modal to display the battery on click*/}
+                        <IonModal isOpen={showBattery} onDidDismiss={() => setShowBattery(false)} cssClass="InnerModal">
+                            <Battery></Battery>
+                        </IonModal>
+                        {/* Right Side: Statistics Battery */}
+                        <IonCol className="RightStats">
+                            <IonCard className="Battery" button={true} onClick={openBatteryModal}>
+                                <IonImg src={batteryGraph} className="ImagePreview" />
+                                <IonCardHeader>
+                                    <IonCardTitle>Battery</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent className="BatteryStats">
+                                    <IonCardTitle className="BatteryPercentage">56%</IonCardTitle>
+                                    <IonIcon src={batteryHalfOutline} className="BatteryIcon"></IonIcon>
+                                </IonCardContent>
+                            </IonCard>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
             </IonContent>
         </IonPage>
     );
---
+++ src/pages/CarModals/Battery.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel,
+} from '@ionic/react';
+
+import './Battery.css';
+
+const Battery: React.FC = () => {
+    return (
+        <IonContent>
+            {/* Tool bar header for Battery page */}
+            <IonHeader>
+                <IonToolbar color="#099648" className="BatteryToolBar">
+                    <IonTitle>Battery</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        {/* Selection of date for stastics */}
+                        <IonCard>
+                            <IonSegment value="today">
+                                <IonSegmentButton value="month" className="DateSegmentButton">
+                                    <IonLabel>Month</IonLabel>
+                                </IonSegmentButton>
+                                <IonSegmentButton value="week" className="DateSegmentButton">
+                                    <IonLabel>Week</IonLabel>
+                                </IonSegmentButton>
+                                <IonSegmentButton value="today" className="DateSegmentButton">
+                                    <IonLabel>Today</IonLabel>
+                                </IonSegmentButton>
+                            </IonSegment>
+                        </IonCard>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol></IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    );
+};
+
+export default Battery;
