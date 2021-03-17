9a8df9e3146264aca92df3cfb4b8596837dec973
Ratik Kapoor <ratikrk@gmail.com>
2021-03-17 00:07:23 -0600

    feat: Add ZMQ to React, callbacks in Redux state

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,14 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.scss';
 import AppContainer from './pages/AppContainer';
+import NetworkManager from './net/NetworkManager';
 
 document.body.classList.add('dark');
 
 const App: React.FC = () => (
     <IonApp>
         <AppContainer />
+        <NetworkManager />
     </IonApp>
 );
 
