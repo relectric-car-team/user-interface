b1601dc11c7419d966830dd906128d0432513c50
Brooke <brooke.kindleman@gmail.com>
2021-03-22 17:17:56 -0600

    Add button to access the settings page to the home page
    - Added a floating action button to the top right of the home page to access the settings page. The settings page pops up in a modal, identically to the other car, navigation, climate, and music pages and can also be closed with either the 'X' button in the top left corner, or by clicking outside of the modal. 
    There is no content in the settings modal; it is an empty page.
    - Created a new indigo-blue colour for use of the toolbar at the top of the settings modal. The colour has been added to the variables.scss document.

---
+++ src/components/SettingsFab.tsx
@@ -0,0 +1,35 @@
+import { IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { menu } from 'ionicons/icons';
+import { Pages } from '../Models/Enums';
+import './TabBar.scss';
+import './SettingsFab.scss';
+import { selectRouter, setPage } from '../features/Routing/RouterStore';
+import React from 'react';
+const SettingsFab: React.FC = () => {
+    const dispatch = useDispatch();
+    const page = useSelector(selectRouter);
+
+    const handleClick = (button: Pages) => {
+        console.log(button);
+        if (button === page) {
+            dispatch(setPage(Pages.Settings));
+        } else {
+            dispatch(setPage(button));
+        }
+    };
+
+    return (
+        <IonFab vertical="top" horizontal="end" slot="fixed">
+            <IonFabButton onClick={() => handleClick(Pages.Settings)} className="FabButton">
+                <IonIcon
+                    icon={menu}
+                    className="TabBarIcon"
+                    color={page === Pages.Settings ? 'relectric-settings' : 'relectric-light'}
+                />
+            </IonFabButton>
+        </IonFab>
+    );
+};
+
+export default SettingsFab;
