5f42a2ec3a9a60f160e06f959f853b045ee907fa
Brooke <brooke.kindleman@gmail.com>
2021-07-30 16:08:39 -0600

    docs: Added documentation for all new redux created for the climate page.

--- src/features/DarkMode/DarkModeStore.tsx
+++ src/features/DarkMode/DarkModeStore.tsx
@@ -1,13 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+/**
+ * Interface for dark mode redux; contains variables needed for dark mode to work correctly.
+ */
 interface DarkMode {
     darkModeActive: boolean;
 }
 
+/**
+ * Initial state the dark/light mode should be set to.
+ */
 const initialState: DarkMode = {
+    /**
+     * Gets intial setting for dark/light mode from the user's device.
+     * If no theme is requested by the user's device, it will default to dark mode.
+     */
     darkModeActive: window.matchMedia('(prefers-color-scheme)').media == 'dark',
 };
 
+/**
+ * Slice for dark mode, provides the reducers necessary to correctly
+ * update the dark and light mode implementation in the app.
+ */
 export const slice = createSlice({
     name: 'darkMode',
     initialState,
@@ -24,6 +38,9 @@ export const slice = createSlice({
     },
 });
 
+/**
+ * Reducer to update the dark mode must be exported with 'slice.actions' as a property.
+ */
 export const { updateDarkMode } = slice.actions;
 
 export default slice.reducer;
