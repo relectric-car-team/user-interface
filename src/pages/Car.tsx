8d51c51e37644d7741ccf699ac391294acc14698
Nicholas Knapton <nickknapton12@gmail.com>
2021-08-01 11:45:55 -0700

    Doors can now open and close and reflected in car model

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -27,7 +27,9 @@ import './InnerModal.scss';
 import { useDispatch } from 'react-redux';
 import { setPage } from '../features/Routing/RouterStore';
 import { Pages } from '../Models/Enums';
-import carAerial from '../assets/car-aerial/car-aerial.svg';
+import carAerial from '../assets/car-aerial/car-aerial-frame.svg';
+import passengerDoor from '../assets/car-aerial/passenger-door.svg';
+import driverDoor from '../assets/car-aerial/driver-door.svg';
 import energyGraph from '../assets/graphs/Energy.jpg';
 import batteryGraph from '../assets/graphs/Battery.jpg';
 import Energy from './CarModals/Energy';
@@ -39,12 +41,22 @@ const StyledIcon = styled(IonIcon).attrs((props: { colour: string }) => ({
     stroke: ${(props) => props.colour};
 `;
 
+const StyledDoor = styled(IonIcon).attrs((props: { colour: string; open: string }) => ({
+    colour: props.colour,
+    open: props.open,
+}))`
+    stroke: ${(props) => props.colour};
+    transform: ${(props) => props.open};
+`;
+
 const Car: React.FC = () => {
     const dispatch = useDispatch();
 
     const [showEnergy, setShowEnergy] = useState(false);
     const [showBattery, setShowBattery] = useState(false);
     const [darkMode] = useState(document.body.classList.contains('dark'));
+    const [PassengerDoorOpen] = useState(false);
+    const [DriverDoorOpen] = useState(false);
 
     const openEnergyModal = function () {
         setShowEnergy(true);
@@ -98,6 +110,20 @@ const Car: React.FC = () => {
                         {/* Middle Section: Car View */}
                         <IonCol className="CenterStats" size="5">
                             <StyledIcon className="AerialView" icon={carAerial} colour={darkMode ? 'white' : 'black'} />
+                            <StyledDoor
+                                className="AerialViewDriverDoor"
+                                icon={driverDoor}
+                                colour={darkMode ? 'white' : 'black'}
+                                open={DriverDoorOpen ? 'rotate(32deg) translate(-37px)' : 'rotate(0deg) translate(0)'}
+                            />
+                            <StyledDoor
+                                className="AerialViewPassengerDoor"
+                                icon={passengerDoor}
+                                colour={darkMode ? 'white' : 'black'}
+                                open={
+                                    PassengerDoorOpen ? 'rotate(-32deg) translate(37px)' : 'rotate(0deg) translate(0)'
+                                }
+                            />
                         </IonCol>
                         {/* Modal to display the battery on click*/}
                         <IonModal isOpen={showBattery} onDidDismiss={() => setShowBattery(false)} cssClass="InnerModal">
