b7186441a208e4413350902e1a7ff3c4e162148f
Brooke <brooke.kindleman@gmail.com>
2021-08-11 20:36:47 -0600

    feat: Inside car temp displayed on home page.
    Indoor car temperature is nowdisplayed on the home page. Position is not
    intended to be permanent, and can be moved upon redesign.
    Fixed formatting issue with temperature symbol toggle on the settings
    page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { IonPage } from '@ionic/react';
 import DateTime from '../components/DateTime';
+import HomePageTemp from '../components/HomePageTemp';
 
 const Home: React.FC = () => {
     return (
         <IonPage>
             <DateTime />
+            <HomePageTemp />
         </IonPage>
     );
 };
