bb231308bb8e1a9ed21b8df18cc618f2f6c4d22f
Brooke <brooke.kindleman@gmail.com>
2021-08-11 20:36:47 -0600

    feat: Inside car temp displayed on home page.
    Indoor car temperature is nowdisplayed on the home page. Position is not
    intended to be permanent, and can be moved upon redesign.
    Fixed formatting issue with temperature symbol toggle on the settings
    page.

---
+++ src/components/HomePageTemp.tsx
@@ -0,0 +1,24 @@
+import { IonContent, IonText, IonTitle } from '@ionic/react';
+
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectDisplayedTemp, selectTempSymbol } from '../app/reducersindex';
+
+import './HomePageTemp.scss';
+
+const HomePageTemp: React.FC = () => {
+    const temperatureSymbol = useSelector(selectTempSymbol);
+    const temperatureValue = useSelector(selectDisplayedTemp);
+    return (
+        <IonContent fullscreen={true} className="content" slot="fixed" scrollY={false}>
+            <IonTitle className="location">Inside</IonTitle>
+
+            <IonTitle className="temperature">
+                {temperatureValue}
+                {temperatureSymbol}
+            </IonTitle>
+        </IonContent>
+    );
+};
+
+export default HomePageTemp;
