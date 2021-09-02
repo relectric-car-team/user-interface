b1110ea66b923c35cb45e7f756e2b29dbe4482f4
Robert Brown <brown.rjb75@gmail.com>
2021-09-01 23:31:30 -0600

    feat: add demo css for ipad pro

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.scss';
 import AppContainer from './pages/AppContainer';
+import './theme/demo.scss';
 
 const App: React.FC = () => (
     <IonApp>
