26cf750190ae43bec70192658e0c1f33523f44a2
dylanleclair <dylan.leclair@icloud.com>
2021-12-05 00:26:04 -0700

    Cleaned up documentation

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { notifications } from '../../app/reducersindex';
 import './Notification.scss';
 import { notifActions } from './NotificationStore';
-import { IonIcon } from '@ionic/react';
 
 // TODO: you need to make it so that the notification:
 // - obeys the light/dark mode settings & make CSS more uniform
 // - add links to router ? or support for this -> will do an example? need a good example. maybe make a fake system notification.
-// - stacking notifications -> WIP
-// - notif categories -> WIP
-// - notification panel (see history) -> future
+// - stacking notifications -> not sure if this is a good idea, since it might take up a lot of the screen. (even on iphone only 1 shows at a time).
+//      -> considering pre-emption of older notifications
+//          -> will need this: https://reactjs.org/docs/animation.html
+// - notif categories -> WIP (ie: make more types)
+// - notification panel (see history) -> future? draft something up in the morning & show during meeting tomorrow.
 //      -> solves problem of having sticky notifications clog the screen
 
 // sticky notification / alerts -> stuff like battery / charge that should persist until cleared
@@ -47,6 +48,7 @@ export class EmptyNotif implements INotif {
         this.duration = duration ? duration : DEFAULT_NOTIFICATION_DURATION;
     }
     renderNotif(onDismiss: () => void): JSX.Element {
+        onDismiss(); // suppress warning
         throw new Error('Method not implemented.');
     }
 }
@@ -92,7 +94,6 @@ export class MusicNotif implements INotif {
 
 const start: Notif = new MusicNotif('', '', '', false);
 
-// map all items from alerts into an IonToast
 const Notification: React.FC = () => {
     const notifs = useSelector(notifications);
     const [notif, setNotif] = useState({ child: start as Notif });
@@ -102,25 +103,35 @@ const Notification: React.FC = () => {
         console.log('Dismiss the notification');
     };
 
-    // select an icon to show based on type?
     const dispatch = useDispatch();
     useEffect(() => {
-        // DESIGN:
-        // all notifications should be collected in linear order.
-        // only one should show at a time.
-        // playing a new notification should not interrupt previous
-        // when a notification expires, the next one will be triggered
-        //  - need to trigger initially
-        //  - on expiry, need to trigger next (if it exists) -> "DAISY CHAIN" ( -> dispatch removal from start)
-
-        // I believe this is called every time a notification action is registered
+        /**
+         * How this works:
+         *
+         *  notifications in state is basically a queue (FIFO) of notifications registered to the system
+         *
+         *  When a notification is first registered, it skips to the inner else statement.
+         *  Then, the notification is set as the active notification, and is set to be rendered in the dom (via show)
+         *
+         *  setTimeout runs the notification expiry code:
+         *      -> if future notifications exist, triggers them after the notification completes
+         *          -> sets next notification as active
+         *          -> removes the previous (already displayed) notification from state
+         *          -> toggles show to make sure DOM recognizes CSS animation needs to play again
+         *       -> else, simply removes the displayed notification from state
+         *
+         * You can think of this as simply checking to see if another notification is in line, and passing it the "baton" if there is.
+         *
+         * We don't want anything to happen when a notification is already being displayed (show is true), since all the logic for
+         * "passing it on" to the next notification is handled in setTimeout
+         */
         if (notifs.length != 0) {
             if (show) {
                 // if a notif is showing, don't do anything (handled in timeout)
             } else {
                 // new notification!
                 setNotif(notifs[0]); // Notifications will be organized like a queue
-                setShow(true); // need to modify so that all notifications are shown / pre-empty one another properly
+                setShow(true); // Set the notification to be rendered (enters DOM -> CSS animation plays)
                 setTimeout(() => {
                     // the notification being displayed expires
                     // check if there is a new one to display!
@@ -131,24 +142,14 @@ const Notification: React.FC = () => {
                         dispatch(notifActions.handleNotification()); // remove the one that was just displayed
                         setShow(true);
                     } else {
+                        // if no new notification, empty the queue so the previous one isn't displayed again!
                         dispatch(notifActions.handleNotification()); // remove the one that was just displayed
                     }
                 }, notif.child.duration * 1000); // duration (s) * 1000 = duration in ms
             }
         }
     }, [notifs]);
-    return show
-        ? // <IonToast
-          //     isOpen={show}
-          //     onDidDismiss={() => setShow(false)}
-          //     duration={200}
-          //     position={'top'}
-          //     message={notif.message}
-          // ></IonToast>
-          notif.child.renderNotif(onDismiss)
-        : null;
+    return show ? notif.child.renderNotif(onDismiss) : null;
 };
-
-// JUST CREATE AN ARRAY OF THE ABOVE ORIGINAL -> each notif has own state! PERFECT!!!!
 
 export default Notification;
