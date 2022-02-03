fadef8a3c844da56da7fe2ab1ff9909d36eb2a21
nickknapton12 <nickknapton12@gmail.com>
2022-02-03 11:44:31 -0700

    Added more reducers and enums to help.

--- src/features/Car/CarStore.tsx
+++ src/features/Car/CarStore.tsx
@@ -7,6 +7,22 @@ export enum DriveMode {
     RACE = 'race',
 }
 
+export enum DoorsEnum {
+    DRIVER = 'driverDoor',
+    PASSENGER = 'passengerDoor',
+    REARDRIVER = 'rearDriverDoor',
+    REARPASSENGER = 'rearPassengerDoor',
+    TRUNK = 'trunk',
+    FRUNK = 'frunk',
+}
+
+export enum TiresEnum {
+    FRONTLEFT = 'driverTire',
+    FRONTRIGHT = 'passengerTire',
+    BACKLEFT = 'rearDriverTire',
+    BACKRIGHT = 'rearPassengerTire',
+}
+
 interface CarState {
     doorStates: {
         driverDoorOpen: boolean;
@@ -14,6 +30,7 @@ interface CarState {
         rearDriverDoorOpen: boolean;
         rearPassengerDoorOpen: boolean;
         trunkOpen: boolean;
+        frunkOpen: boolean;
     };
 
     tirePressure: {
@@ -23,12 +40,6 @@ interface CarState {
         backRight: number;
     };
 
-    /* Note: battery level is one thing, however there is a question on how we should
-     *       store past battery level for the utilization graph. This question is mostly
-     *       how long should we store the percentage from. Should we store the last week
-     *       worth of data while updating the graph maybe once every minute? Should we maybe
-     *       store data from the last start? This would prob be the easiest to implement.
-     */
     batteryLevel: Array<number>;
     powerDraw: Array<number>;
 
@@ -47,6 +58,7 @@ const initialState: CarState = {
         rearDriverDoorOpen: false,
         rearPassengerDoorOpen: false,
         trunkOpen: false,
+        frunkOpen: false,
     },
     tirePressure: {
         frontLeft: 0,
@@ -65,6 +77,13 @@ export const slice = createSlice({
     name: 'car',
     initialState,
     reducers: {
+        /**
+         * Updates the state of a door in the store.
+         * @param state Current redux state.
+         * @param action The action to perform, this should have action.payload.door indicating the door to be changed (options are: 'driverDoor', 'passengerDoor',
+         *               'rearDriverDoor', 'rearPassengerDoor', 'trunk', 'frunk') but enums are also available for this. The second thing that should be passed in is the new door
+         *               state, action.payload.open should either be true or false.
+         */
         updateDoors: (state, action) => {
             switch (action.payload.door) {
                 case 'driverDoor':
@@ -78,6 +97,78 @@ export const slice = createSlice({
                     break;
                 case 'rearDriverDoor':
                     state.doorStates.rearDriverDoorOpen = action.payload.open;
+                    break;
+                case 'trunk':
+                    state.doorStates.trunkOpen = action.payload.open;
+                    break;
+                case 'frunk':
+                    state.doorStates.frunkOpen = action.payload.open;
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        /**
+         * Updates store to reflect current battery percentage.
+         * @param state current redux state.
+         * @param action the action to perform, this should be action.payload.batteryLevel and should contain the current battery level.
+         */
+        updateBatteryLevel: (state, action) => {
+            // configered to record the last weeks worth of data every 30 mins.
+            if (state.batteryLevel.length >= 7 * 24 * 30) {
+                state.batteryLevel.shift();
+            }
+
+            state.batteryLevel.push(action.payload.batteryLevel);
+        },
+
+        /**
+         * Updates store to reflect current power draw.
+         * @param state current redux state.
+         * @param action the action to perform, this should be action.payload.powerDraw and should contain the current powerDraw.
+         */
+        updatePowerDraw: (state, action) => {
+            // configered to record the last weeks worth of data every 30 mins.
+            if (state.powerDraw.length >= 7 * 24 * 30) {
+                state.powerDraw.shift();
+            }
+
+            state.powerDraw.push(action.payload.powerDraw);
+
+            const sum = state.powerDraw.reduce((a, b) => a + b, 0);
+            state.averagePowerDraw = sum / state.powerDraw.length;
+        },
+
+        /**
+         * Updates the current speed in the store.
+         * @param state current redux state.
+         * @param action the action to perform, this should contain action.payload.speed and should be the speed the car is moving at.
+         */
+        updateCurrentSpeed: (state, action) => {
+            state.currentSpeed = action.payload.speed;
+        },
+
+        /**
+         * Updates the pressure of a tire in the store.
+         * @param state Current redux state.
+         * @param action The action to perform, this should have action.payload.tire indicating the tire to be changed (options are: 'drivertire', 'passengertire',
+         *               'rearDriverTire', 'rearPassengerTire') but enums are also available for this. The second thing that should be passed in is the new tire
+         *               pressure, action.payload.pressure should hold this.
+         */
+        updateTirePressure: (state, action) => {
+            switch (action.payload.tire) {
+                case 'driverTire':
+                    state.tirePressure.frontLeft = action.payload.pressure;
+                    break;
+                case 'passengerTire':
+                    state.tirePressure.frontRight = action.payload.pressure;
+                    break;
+                case 'rearPassengerTire':
+                    state.tirePressure.backRight = action.payload.pressure;
+                    break;
+                case 'rearDriverTire':
+                    state.tirePressure.backLeft = action.payload.pressure;
                     break;
                 default:
                     break;
