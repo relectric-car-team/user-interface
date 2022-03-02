5e3c37560ce66a564cfc42d7f6117af4cdece2d4
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-02 11:46:03 -0700

    fix: Moved homepage background formatting to its own file, not in temperature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { IonPage } from '@ionic/react';
 import DateTime from '../components/DateTime';
 import HomePageTemp from '../components/HomePageTemp';
+import './Home.scss';
 
 const Home: React.FC = () => {
-    return (
-        <IonPage>
-            <DateTime />
-            <HomePageTemp />
-        </IonPage>
-    );
+    return <IonPage className="homePage" />;
 };
 
 export default Home;
