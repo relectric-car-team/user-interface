54816bbd20222e72da8b2a2038b93c9ae62feb74
nickknapton12 <nickknapton12@gmail.com>
2022-03-11 21:01:11 -0700

    Car Component

---
+++ src/components/CarComponent.tsx
@@ -0,0 +1,69 @@
+import { IonIcon } from '@ionic/react';
+import React from 'react';
+import Body from '../assets/car-aerial/Body.svg';
+import './CarComponent.scss';
+import Headlights from '../assets/car-aerial/HeadLights.svg';
+import TailLights from '../assets/car-aerial/TailLights.svg';
+import DriverDoor from '../assets/car-aerial/LeftTopDoor.svg';
+import PassengerDoor from '../assets/car-aerial/TopRightDoor.svg';
+import RearDriverDoor from '../assets/car-aerial/BottomLeftDoor.svg';
+import RearPassengerDoor from '../assets/car-aerial/BottomRightDoor.svg';
+import CarFrame from '../assets/car-aerial/Union.svg';
+import { DoorsEnum } from '../features/Car/CarStore';
+
+const Car: React.FC = () => {
+    function doorToggle(door: DoorsEnum) {
+        switch (door) {
+            case DoorsEnum.DRIVER:
+                document.getElementById('driverDoor')?.classList.toggle('driverSideRotate');
+                break;
+            case DoorsEnum.PASSENGER:
+                document.getElementById('passengerDoor')?.classList.toggle('passengerSideRotate');
+                break;
+            case DoorsEnum.REARDRIVER:
+                document.getElementById('rearDriverDoor')?.classList.toggle('passengerSideRotate');
+                break;
+            case DoorsEnum.REARPASSENGER:
+                document.getElementById('rearPassengerDoor')?.classList.toggle('passengerSideRotate');
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div className="carComponentWrapper">
+            <IonIcon icon={CarFrame} className="carFrame" />
+            <IonIcon
+                icon={DriverDoor}
+                id="driverDoor"
+                className="carDriverDoor"
+                onClick={() => doorToggle(DoorsEnum.DRIVER)}
+            />
+            <IonIcon
+                icon={PassengerDoor}
+                id="passengerDoor"
+                className="carPassengerDoor"
+                onClick={() => doorToggle(DoorsEnum.PASSENGER)}
+            />
+            <IonIcon
+                icon={RearDriverDoor}
+                id="rearDriverDoor"
+                className="carRearDriverDoor"
+                onClick={() => doorToggle(DoorsEnum.REARDRIVER)}
+            />
+            <IonIcon
+                icon={RearPassengerDoor}
+                id="rearPassengerDoor"
+                className="carRearPassengerDoor"
+                onClick={() => doorToggle(DoorsEnum.REARPASSENGER)}
+            />
+            <IonIcon icon={Body} className="carBody" />
+            <IonIcon icon={Headlights} className="carHeadLights" />
+            <IonIcon icon={CarFrame} className="carFrame" />
+            <IonIcon icon={TailLights} className="carTailLights" />
+        </div>
+    );
+};
+
+export default Car;
